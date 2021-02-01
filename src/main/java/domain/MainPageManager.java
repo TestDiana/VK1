d7e4dfd0fb2fb6a73584f1839d7f8421aaab747f
Diana Vafina <Dianochka.25@mail.ru>
2021-02-01 14:58:58 +0300

    updated MainPageManager.java ang added tag

--- src/main/java/domain/MainPageManager.java
+++ src/main/java/domain/MainPageManager.java
@@ -4,6 +4,7 @@ import domain.CommentsInfo;
 
 public class MainPageManager {
     private int iconOfGroup;
+    private int groupName;
     private Post post;
     private CommentsInfo commentsInfo;
 
